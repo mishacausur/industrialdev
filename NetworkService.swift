5c0139fd44d25ffd9c9c9c3aa284a46007b16526
MishaCausur <mishacausur@mail.ru>
2021-05-05 21:55:01 +0500

    network created

---
+++ NetworkService.swift
@@ -0,0 +1,22 @@
+//
+//  NetworkService.swift
+//  Navigation
+//
+//  Created by Misha Causur on 05.05.2021.
+//  Copyright © 2021 Artem Novichkov. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkService {
+    
+    private static let dataSession = URLSession.shared
+    
+    static func dataTask(){
+        
+    }
+    
+    
+    
+    
+}
