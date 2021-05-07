4241670971d739cc842fb85ee09051da6d10aab3
MishaCausur <mishacausur@mail.ru>
2021-05-08 00:58:53 +0500

    fixed

--- NetworkService.swift
+++ NetworkService.swift
@@ -10,13 +10,21 @@ import Foundation
 
 struct NetworkService {
     
-    private static let dataSession = URLSession.shared
+    static var appConfigutator: AppConfiguration?
     
-    static func dataTask(){
-        
+    static let dataSession = URLSession.shared
+    
+    static func dataTask(url: URL, completion: @escaping (String?) -> Void) {
+        NetworkService.dataSession.dataTask(with: url) { ( data, response, error )  in
+            guard let taskResponse = response as? HTTPURLResponse, taskResponse.statusCode == 200 else { return }
+            print(taskResponse.allHeaderFields, taskResponse.statusCode)
+            if data != nil { completion(String(data: data!, encoding: .utf8))}}.resume()
+        }
     }
-    
-    
-    
-    
+
+
+enum AppConfiguration {
+    case first(URL)
+    case second(URL)
+    case third(URL)
 }
